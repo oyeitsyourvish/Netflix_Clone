bee844a292f567277c9b94738a0b79f00c9252b0
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-09-10 08:39:41 +0530

    Added Nuget packages and Added ApplicationDb

--- NetflixClone/Program.cs
+++ NetflixClone/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using NetflixClone.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//Registers our DbContext with Dependency Injection.
+builder.Services.AddDbContext<ApplicationDbContext>
+//Use SQL Server as the database provider.
+(options => options.UseSqlServer
+//Get the connection string from appsettings.json.
+(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
