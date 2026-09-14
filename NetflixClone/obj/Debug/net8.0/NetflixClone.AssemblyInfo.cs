e537910c84040a708b08a8864afa8d06a11a1b5b
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-09-14 09:02:52 +0530

    Adde _Layout page for register, login, logout

--- NetflixClone/obj/Debug/net8.0/NetflixClone.AssemblyInfo.cs
+++ NetflixClone/obj/Debug/net8.0/NetflixClone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetflixClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a166995dd3378793ea296442df7182a07d93f1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3d9714995dfc79b093905d89e93e00767429cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetflixClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetflixClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
