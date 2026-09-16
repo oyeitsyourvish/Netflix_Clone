28a0c4c4413d70ded695f367d99014e86485ce03
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-09-16 16:32:59 +0530

    Make here section responsive and changes in home page ui

--- NetflixClone/obj/Debug/net8.0/NetflixClone.AssemblyInfo.cs
+++ NetflixClone/obj/Debug/net8.0/NetflixClone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetflixClone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7664b421e1c3be2561b25d8c7ec496b69762c99d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c08ac9d90c94c78219f792fb107bca5304df6e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetflixClone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetflixClone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
